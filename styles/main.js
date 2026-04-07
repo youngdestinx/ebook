0f64accca4426abaa34a729dd6c11cee41a700ac
Destiny Linus <youngdestinx@gmail.com>
2026-04-07 19:57:02 +0100

    Initial commit: Paid to Teach site.

---
+++ styles/main.js
@@ -0,0 +1,19 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const faders = document.querySelectorAll('.fade-in');
+
+    const options = {
+        threshold: 0.1, // when 10% of element is visible
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target); // fade in only once
+            }
+        });
+    }, options);
+
+    faders.forEach(fader => observer.observe(fader));
+});
